38f711c3a049c23b47ea551201a4f14fd3adb69e
jncompany <jhj00882@gmail.com>
2017-04-12 16:12:35 +0900

    fff

--- src/main/java/com/jncompany/SiteParserApplication.java
+++ src/main/java/com/jncompany/SiteParserApplication.java
@@ -4,13 +4,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class SiteParserApplication {
+public class SiteParserApplication extends SpringBootServletInitializer{
 
 	private static final Logger log = LoggerFactory.getLogger(SiteParserApplication.class);
+	
+	 @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SiteParserApplication.class);
+    }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SiteParserApplication.class, args);
